5a34d91f25264057caf51e7d0ff2f3f0467c5f9b
Tuan <tehtt91@gmail.com>
2020-03-02 13:57:56 +0100

    some new journal files

---
+++ NXOpen/create_measure_result.py
@@ -0,0 +1,34 @@
+﻿# NX 12.0.2.9
+# Journal created by tuanat on Mon Mar  2 13:41:07 2020 W. Europe Standard Time
+#
+import math
+import NXOpen
+import NXOpen.CAE
+def main() :
+
+    theSession  = NXOpen.Session.GetSession()
+    workSimPart = theSession.Parts.BaseWork
+    displaySimPart = theSession.Parts.BaseDisplay
+
+
+
+    simSimulation1 = workSimPart.FindObject("Simulation")
+    simSolution1 = simSimulation1.FindObject("Solution[Solution 1]")
+    simResultReference1 = simSolution1.Find("Structural")
+    resultManager1 = theSession.ResultManager
+    solutionResult1 = resultManager1.FindObject("SolutionResult[model1_fem1_sim1.sim_Solution 1]")
+    loadcase1 = solutionResult1.Find("Loadcase[1]")
+    iteration1 = loadcase1.Find("Iteration[1]")
+    ptype1 = NXOpen.CAE.Result.Type(NXOpen.CAE.Result.Quantity.Stress, NXOpen.CAE.Result.Location.ElementNodal, NXOpen.CAE.Result.Section.NotApplicable)
+    resultMeasureResultOptions1 = simSimulation1.ResultMeasures.CreateNewResultOptions(simSolution1, simResultReference1, loadcase1, iteration1, False, NXOpen.CAE.Iteration.Null, ptype1, NXOpen.CAE.Result.Component.VonMises)
+    unit1 = workSimPart.UnitCollection.FindObject("StressNewtonPerSquareMilliMeter")
+    resultMeasureResultOptions1.SetUnit(unit1)
+    resultMeasureResultOptions1.SetPlynum(0)
+    resultMeasureResultOptions1.SetOperation(NXOpen.CAE.ResultMeasure.Operation.Maximum)
+    resultMeasure1 = simSimulation1.ResultMeasures.CreateResultMeasureNew(resultMeasureResultOptions1, None, "as1newre234sultmeasure")
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ NXOpen/measure_results2.py
@@ -0,0 +1,23 @@
+﻿# NX 12.0.2.9
+# Journal created by tuanat on Mon Mar  2 12:39:43 2020 W. Europe Standard Time
+#
+import math
+import NXOpen
+import NXOpen.CAE
+def main() :
+
+    theSession  = NXOpen.Session.GetSession()
+    workSimPart = theSession.Parts.BaseWork
+
+    simSimulation1 = workSimPart.FindObject("Simulation")
+    objects1 = [NXOpen.CAE.ResultMeasure.Null] * 1
+    resultMeasure1 = simSimulation1.ResultMeasures.Find("NXOpen.CAE.ResultMeasure[test]")
+    objects1[0] = resultMeasure1
+
+    simSimulation1.ResultMeasures.UpdateMeasures(objects1)
+
+    theNxMessageBox = NXOpen.UI.GetUI().NXMessageBox
+    theNxMessageBox.Show("Bodies",NXOpen.NXMessageBoxDialogType.Information, str(resultMeasure1.Result))
+
+if __name__ == '__main__':
+    main()
