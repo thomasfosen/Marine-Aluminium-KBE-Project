0aa3ff762e4745dcd6f6a76563e48bd672aabaa6
thomasfosen <thomalf@stud.ntnu.no>
2020-03-05 12:43:25 +0100

    Some fixes

--- Web Server/app.py
+++ Web Server/app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, request
 
-import json
-import re
-import os
-import random
-import time
 
-#from Reasoner import Reasoner
 
 app = Flask(__name__)
 
@@ -16,17 +10,10 @@ app = Flask(__name__)
 def main():
 
     customer_form = request.form
-    
+
     return render_template('User.html')
-
-
-
 
 
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port = '5000')
-
-
-
-#  #<form class="" action="{{ url_for('User') }}" method="post">
