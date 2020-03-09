f26012b0f64c0aa3a1566c3a46d122ddba180e10
thomasfosen <thomalf@stud.ntnu.no>
2020-03-09 09:54:27 +0100

    Updates

--- Web Server/communicator.py
+++ Web Server/communicator.py
@@ -1,17 +1,21 @@
 import asyncio
-import websockets
+#import websockets
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', metavar='Command', type=str)
-parser.add_argument('-n', metavar='Name', type=str, const=None, default=None)
-parser.add_argument('-t', metavar='Target', type=str, const=None, default=None)
+parser.add_argument('-f', metavar='Force', type=float, const=None, default=None)
+parser.add_argument('-t', metavar='Torque', type=float, const=None, default = None)
 
 args = parser.parse_args()
 cmd = args.c
-name = args.n
-target = args.t
+force = args.f
+torque = args.t
 
+
+#print(torque, force, cmd)
+
+"""
 async def message():
 	async with websockets.connect("ws://localhost:1234") as socket:
 
@@ -27,9 +31,11 @@ async def message():
 		elif cmd == 'refresh':
 			await socket.send(cmd)
 
+		elif cmd == 'optimize':
+"""
 
 
-asyncio.get_event_loop().run_until_complete(message())
+#asyncio.get_event_loop().run_until_complete(message())
 
 
 # The process needs to be launched from a seperate process thread. Doing this inside a flask application wasn't as straight forward as initially thought..
